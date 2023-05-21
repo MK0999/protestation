0bd7ec4e0060bf7f8cf0cd107093b114943c3b5b
Kirandeep <kirandeep1195@gmail.com>
2023-05-21 18:15:07 -0400

    ADDED CODE

---
+++ assests/style.js
@@ -0,0 +1,74 @@
+var questions = [ 
+    {
+    Num: 1,
+    Quest:  "which of the following is not javascript data types?",
+Option: [
+    " Null type" ,
+       " Undefined type" ,
+        "Number type" ,
+        "All of the mentioned" ,
+],
+
+Answer: " All of the mentioned"
+},
+{
+Num: 2,
+Quest: "Which of the following can be used to call a javaScript code Snippet",
+Option: [
+    "Function/Method",
+          "Preprocessor",
+          "Triggering event",
+          "RMI"
+],
+Answer: "Function/Method",
+},
+{
+    Num : 3,
+    Quest : "Javascript is an _______ language?",
+    Option :
+    ["Object-orientsd",
+              "Object-based",
+              "Procedural",
+              "None of above" 
+],
+     Answer : "Object-Oriented " 
+} ,       
+ {
+    Num : 4,
+    Quest: "Upon encountering empty statements, what does the Javascript Interpreter do?",
+    Option : [
+        "Throws an error",
+    "Ignores the statement",
+    "Gives a warning",
+    "None of above"
+    ],
+    Answer :"Ignores the statement",
+ },
+ {
+    Num: 5,
+    Quest : "  What keyword is used to check whether a given property is valid or not?",
+    Option :[
+        "in",
+    "is in",
+    "exists",
+    "lies"
+    ],
+     Answer: "in"
+
+},
+]  
+var Quest_ion = document.getElementById("questions");
+var Opt_ion = document.getElementById("options");
+var Ans_wer = document.getElementById("answer");
+var Start_btn = document.getElementById("strt-btn");
+var Nxt_btn = document.querySelectorAll("next");
+    
+    
+    
+      
+    
+    
+    
+    
+
+
